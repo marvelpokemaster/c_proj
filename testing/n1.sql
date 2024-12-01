3fe9b9f431d412d20c3e8156fe189d8a76b87b92
Adarsh Binu <marvelpokemaster@gmail.com>
2024-12-01 18:48:49 +0530

    modified attributes for vehicles

--- testing/n1.sql
+++ testing/n1.sql
@@ -1,21 +1,14 @@
 CREATE TABLE vehicles (
     vehicle_id SERIAL PRIMARY KEY,
     name VARCHAR(100),
-    details TEXT,
-    price NUMERIC
+    price NUMERIC,
+    type VARCHAR(50),
+    height NUMERIC,
+    width NUMERIC,
+    color VARCHAR(50)
 );
 
-CREATE TABLE customers (    phone VARCHAR(15)
-);
-
-CREATE TABLE sales (
-    sale_id SERIAL PRIMARY KEY,
-    vehicle_id INT REFERENCES vehicles(vehicle_id),
-    customer_id INT REFERENCES customers(customer_id),
-    amount_paid NUMERIC,
-    full_payment BOOLEAN,
-    sale_date TIMESTAMP DEFAULT NOW()
-);
+CREATE TABLE customers (
     customer_id SERIAL PRIMARY KEY,
     name VARCHAR(100),
     address TEXT,
